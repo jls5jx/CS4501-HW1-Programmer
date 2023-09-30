2489503e55af5bdf9c5c48aa26a276b511939a09
Jasmine Senesombath <123671917+jls5jx@users.noreply.github.com>
2023-09-29 20:43:24 -0400

    Update gradescript.js

--- gradescript.js
+++ gradescript.js
@@ -18,6 +18,8 @@ hoverTooltips.forEach((tooltip) => {
     });
 });
 
+const backButton = document.getElementById('back');
+
 backButton.addEventListener('click', function () {
     window.location.href = 'index.html'; 
 });
